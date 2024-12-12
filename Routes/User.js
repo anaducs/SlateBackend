7ee71c6d4c4945600c0867a71081c4101af40679
Anandu C S <csanandu1998@gmail.com>
2024-12-12 23:26:05 +0530

    added signup and login routes also added password validation

--- Routes/User.js
+++ Routes/User.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const route = express.Router();
+const userModel = require('../Models/userModel');
 
-route.post('/register',(req,res)=>{
-    const{name,email,password}=req.body;
+route.post('/register',async (req,res)=>{
+const{name,email,password}=req.body;
 if(!name || !email|| !password){
-    console.log('error');
-    
+    return;
 }
-console.log({name,email,password});
-res.status(200).json({message:"complete"});
+try{
+const user = new userModel({
+    name,email,password
+});
 
+const savedUser = await user.save();
+
+res.status(201).json({user:{name:savedUser.name}});
+
+}catch(e){
+res.status(409).json({msg:"email already exits"});
+
+}
 
 });
 
+route.post('/login',async(req,res)=>{
+    const{email,password}=req.body;
+    try{
+        const t = email;
+        const user = await userModel.findOne({email});
+        
+        if(!user){
+           return res.status(404).json({msg:"email not registered"});
+        }
+        if(!password===user.password){
+            return res.status(401).json({msg:"INvalid pASSWORD"});
+        }
+        console.log("logged in");
+        
+
+
+        
+    }catch(err){
+            console.log("catch",err)
+    }
+})
+
 route.get('/',(req,res)=>{
-    res.send('hi');
 })
 
 module.exports = route;
