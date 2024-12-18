02767b33bf339c5cad2e0c0861023dd1b43c7d50
Anandu C S <csanandu1998@gmail.com>
2024-12-18 21:46:23 +0530

    added socket io and set up client server communication and openeded some liseting events

--- Models/Document.js
+++ Models/Document.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const documentSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require:true,
+const documentSchema = new mongoose.Schema(
+  {
+    _id: String,
+    data: Object,
+    name: {
+      type: String,
+      default: "Untitled",
     },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        require:true,
-        ref:'user'
-    },
-    
-},{timestamps:true});
 
-module.exports = mongoose.model('Document',documentSchema);
+    // userId:{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     require:true,
+    //     ref:'user'
+    // },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Document", documentSchema);
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookie = require("cookie-parser");
+const http = require("http");
 
+const app = express();
+const port = process.env.PORT || 5000;
 const uri = process.env.MONGO_URL;
-const port = process.env.PORT;
 
-const userRoute = require("./Routes/User");
-
-//middleware
+const userRoute = require("./Routes/User"); 
+const initializeSocket = require("./Service/DocumentService"); 
+// Middleware
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -20,22 +21,26 @@ app.use(
 app.use(express.json());
 app.use(cookie());
 
-//routes
-
+// Routes
 app.use("/api/users", userRoute);
 
-//db
-
+// Database connection
 mongoose
   .connect(uri)
   .then(() => {
-    console.log("db connected!");
+    console.log("DB connected!");
   })
   .catch((e) => {
-    console.log(e.errorResponse);
+    console.error("Error connecting to DB:", e);
   });
 
-//server
-app.listen(port || 5000, () => {
-  console.log(`server started on ${port}`);
+
+const server = http.createServer(app);
+
+
+initializeSocket(server);
+
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
